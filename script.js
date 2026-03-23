fbb1548c2d835a5d08f16c65d3dc613877e8855c
yassinwhibi-wq <yassin.whibi@etudiant-fst.utm.tn>
2026-03-23 23:18:04 +0100

    compléter le reste de les fonctionnalités de génerer un mot de passe et copier et sauvegarder dans l'historique

--- script.js
+++ script.js
@@ -18,3 +18,60 @@ const barre = document.getElementById("barre");
 
 const historique = document.getElementById("listeHistorique");
 
+// caractères
+const MAJ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const MIN = "abcdefghijklmnopqrstuvwxyz";
+const NUM = "0123456789";
+const SYM = "!@#$%^&*()_+[]{}";
+
+// afficher taille
+taille.oninput = () => {
+  tailleValeur.textContent = taille.value;
+};
+
+// générer mot de passe
+btnGen.onclick = () => {
+  let chars = "";
+  if(maj.checked) chars += MAJ;
+  if(min.checked) chars += MIN;
+  if(chiffre.checked) chars += NUM;
+  if(symbole.checked) chars += SYM;
+
+  if(chars === "") return alert("Choisis au moins une option");
+
+  let pass = "";
+  for(let i=0;i<taille.value;i++){
+    pass += chars[Math.floor(Math.random()*chars.length)];
+  }
+
+  resultat.value = pass;
+
+  // DETAILS
+  longueurTexte.textContent = pass.length;
+
+  let score = 0;
+  if(pass.length > 8) score += 30;
+  if(pass.length > 12) score += 30;
+  if(symbole.checked) score += 20;
+  if(chiffre.checked) score += 20;
+
+  barre.value = score;
+
+  if(score < 40) niveauTexte.textContent = "Faible";
+  else if(score < 70) niveauTexte.textContent = "Moyen";
+  else niveauTexte.textContent = "Fort";
+
+  tempsTexte.textContent = score < 40 ? "Rapide" : score < 70 ? "Quelques heures" : "Très long";
+
+  // HISTORIQUE
+  const li = document.createElement("li");
+  li.textContent = pass;
+  historique.prepend(li);
+};
+
+// copier
+btnCopy.onclick = () => {
+  if(resultat.value === "") return;
+  navigator.clipboard.writeText(resultat.value);
+  alert("Copié !");
+};
