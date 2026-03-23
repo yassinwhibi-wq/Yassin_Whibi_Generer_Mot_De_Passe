ab3bd0e7272180194382f2bcb805404c734c0e36
yassinwhibi-wq <yassin.whibi@etudiant-fst.utm.tn>
2026-03-23 23:15:51 +0100

    dans js , créer tous les variables de GetElements pour tous les champs importants

---
+++ script.js
@@ -0,0 +1,20 @@
+// éléments
+const taille = document.getElementById("taille");
+const tailleValeur = document.getElementById("tailleValeur");
+const resultat = document.getElementById("resultat");
+
+const maj = document.getElementById("maj");
+const min = document.getElementById("min");
+const chiffre = document.getElementById("chiffre");
+const symbole = document.getElementById("symbole");
+
+const btnGen = document.getElementById("generer");
+const btnCopy = document.getElementById("copier");
+
+const longueurTexte = document.getElementById("longueurTexte");
+const niveauTexte = document.getElementById("niveauTexte");
+const tempsTexte = document.getElementById("tempsTexte");
+const barre = document.getElementById("barre");
+
+const historique = document.getElementById("listeHistorique");
+
